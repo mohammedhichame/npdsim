b781c8959b42b29b1c33a9d1611400e14add9930
Hichame <res.mhichame@gmail.com>
2025-03-22 10:27:40 +0100

    Update to the documentation of the function demand_sim()

--- R/demand_sim.R
+++ R/demand_sim.R
@@ -18,7 +18,10 @@
 #' @import dplyr
 #'
 #' @examples
-#' demand_sim(products_number=100,periods_number=20,shape_number=5,level_number=20,
+#' demand_sim(products_number=100,periods_number=20,shape_number=5, level_number=20,
+#' level_range=1000:10000,noise_cv=0.05)
+#'
+#' demand_sim(products_number=100,periods_number=20,shape_number=5, shape_type="bass", level_number=20,
 #' level_range=1000:10000,noise_cv=0.05)
 demand_sim <- function(products_number,periods_number,shape_number, shape_type="random",
                        level_number,level_range=1000:10000,
