bed302eec0f4130a4c27cb3cc12f90229b424a1a
Hichame <res.mhichame@gmail.com>
2025-04-06 15:18:32 +0200

    Update the function npd_data_sim and create its tests.

--- R/npd_data_sim.R
+++ R/npd_data_sim.R
@@ -17,60 +17,96 @@
 #' @param level_number Number of generic levels
 #' @param level_range Range of values from which the level is sampled
 #' @param noise_cv The coefficient of variation of the noise added to the simulated sales
+#' @param attribute_type Type of relationship between attributes and shape and level.
+#'       There can be independent attributes or dependent attributes. attribute_type takes
+#'       one of the two values: "dep" and "ind". Check `attribute_sim_dep` and `attribute_sim_dep`.
+#' @param product_shapes_and_levels A numeric dateframe of three columns: product_id, assigned_shape and assigned_level
+#' @param attributes_number The number of attributes
+#' @param shape_attributes_number The number of attributes assigned to shape
+#' @param level_attributes_number The number of attributes assigned to level
 #'
-#' @return A date frame that contains the following columns: product_id, shape and assigned_shape,
-#'          level and assigned_level, demand_wn (demand without noise, not rounded),
-#'          noise and demand. demand is the rounded value of the Max
-#'          between (demand_wn+noise) and 0
+#'
+#' @return A date frame that contains the following columns: product_id, demand and attributes.
 #' @export
 #'
 #' @import dplyr
 #' @import tidyr
 #'
 #' @examples
-#' npd_data_sim(products_number=100,periods_number=20,shape_number=5, level_number=20)
+#' npd_data_sim(products_number=100,
+#' periods_number=30,
+#' shape_number=5,
+#' level_number=20)
 #'
-#' npd_data_sim(products_number=100,periods_number=20,shape_number=5, shape_type="bass", level_number=20,
-#' level_range=1000:10000,noise_cv=0.05)
-npd_data_sim <- function(products_number,periods_number,shape_number, shape_type="random",
-                       level_number,level_range=1000:10000,
+#' npd_data_sim(products_number=100,
+#' periods_number=20,
+#' shape_number=5,
+#' shape_type="bass",
+#' level_number=20,
+#' level_range=1000:10000,
+#' noise_cv=0.05,
+#' attribute_type="ind",
+#' attributes_number=15,
+#' shape_attributes_number=7,
+#' level_attributes_number=5)
+#'
+#'
+npd_data_sim <- function(products_number,
+                         periods_number,
+                         shape_number,
+                         shape_type="random",
+                       level_number,
+                       level_range=1000:10000,
                        noise_cv=0.05,
-                       attributes_number,
-                       shape_attributes_number,
-                       level_attributes_number) {
+                       attribute_type="ind",
+                       attributes_number=10,
+                       shape_attributes_number=5,
+                       level_attributes_number=3) {
 
 
+demand_np <- demand_sim(products_number,
+             periods_number,
+             shape_number,
+             shape_type,
+             level_number,
+             level_range,
+             noise_cv)
 
-demand_np <- demand_sim(products_number = ,
-             periods_number = ,
-             shape_number = ,
-             shape_type = ,
-             level_number = ,
-             level_range = ,
-             noise_cv = )
+demand_np1 <- dplyr::select(demand_np, product_id, assigned_shape, assigned_level)
 
+demand_np1 <- dplyr::distinct(demand_np1)
 
   if (attribute_type=="dep") {
 
-    attribute_sim_dep(product_shapes_and_levels,
+    attribute_np <- attribute_sim_dep(product_shapes_and_levels=demand_np1,
                       attributes_number,
                       shape_attributes_number,
                       level_attributes_number)
-
-
   }
 
 
   if (attribute_type=="ind") {
 
-    attribute_sim_ind(product_shapes_and_levels,
+    attribute_np <- attribute_sim_ind(product_shapes_and_levels=demand_np1,
                       attributes_number,
                       shape_attributes_number,
                       level_attributes_number)
 
-
   }
 
+
+demand_np <- dplyr::left_join(demand_np,
+                              attribute_np,
+                              by = join_by(product_id,
+                                           assigned_shape,
+                                           assigned_level))
+
+dplyr::select(demand_np,-c(assigned_shape,
+                             assigned_level,
+                             level,
+                             shape,
+                             demand_wn,
+                             noise))
 
 
 }
---
+++ tests/testthat/test-npd_data_sim.R
@@ -0,0 +1,36 @@
+test_that("example 1 works", {
+
+ abc <- npd_data_sim(products_number=100,
+ periods_number=30,
+ shape_number=5,
+ level_number=20)
+
+    expect_s3_class(abc, "data.frame")
+  expect_equal(dim(abc), c(3000,13))
+  expect_type(abc$demand, "double")
+  expect_type(abc$time, "double")
+})
+
+
+
+
+test_that("example 2 works", {
+
+  abc <- npd_data_sim(products_number=100,
+               periods_number=20,
+               shape_number=5,
+               shape_type="bass",
+               level_number=20,
+               level_range=1000:10000,
+               noise_cv=0.05,
+               attribute_type="ind",
+               attributes_number=15,
+               shape_attributes_number=7,
+               level_attributes_number=5)
+
+
+  expect_s3_class(abc, "data.frame")
+  expect_equal(dim(abc), c(2000,18))
+  expect_type(abc$demand, "double")
+  expect_type(abc$time, "double")
+})
