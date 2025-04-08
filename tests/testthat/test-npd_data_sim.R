dd97a280e1d82a2dde1265af75a4b3bb6c22b3b5
Hichame <res.mhichame@gmail.com>
2025-04-09 00:28:11 +0200

    test_that simulating data for one product works.

--- tests/testthat/test-npd_data_sim.R
+++ tests/testthat/test-npd_data_sim.R
@@ -32,3 +32,25 @@ test_that("example 2 works", {
   expect_equal(dim(abc), c(2000,18))
   expect_type(abc$demand, "double")
 })
+
+
+
+test_that("simulating data for one product works", {
+
+  abc <- npd_data_sim(products_number=1,
+                      periods_number=20,
+                      shape_number=5,
+                      shape_type="bass",
+                      level_number=20,
+                      level_range=1000:10000,
+                      noise_cv=0.05,
+                      attribute_type="ind",
+                      attributes_number=15,
+                      shape_attributes_number=7,
+                      level_attributes_number=5)
+
+
+  expect_s3_class(abc, "data.frame")
+  expect_equal(dim(abc), c(20,18))
+  expect_type(abc$demand, "double")
+})
