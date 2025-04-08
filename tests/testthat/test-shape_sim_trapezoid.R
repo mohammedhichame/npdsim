18192f2c82fdcc6d2512c2e89de18e1c7d07c5cb
Hichame <res.mhichame@gmail.com>
2025-04-08 15:28:28 +0200

    Add tests for different functions

---
+++ tests/testthat/test-shape_sim_trapezoid.R
@@ -0,0 +1,8 @@
+test_that("example 1 works", {
+
+  abc <- shape_sim_trapezoid(periods_number=20, shape_number=5)
+
+  expect_s3_class(abc, "data.frame")
+  expect_equal(dim(abc), c(105,3))
+  expect_type(abc$shape, "double")
+})
